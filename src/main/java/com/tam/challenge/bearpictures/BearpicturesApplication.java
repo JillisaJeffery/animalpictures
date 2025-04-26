ce26f4126f09016d952f8f19e2454c3de9a9f010
jillisa <jillisa@Jillisas-MBP-6.lan>
2025-04-26 00:43:14 -0500

    Initial commit - TAM Tech Challenge

---
+++ src/main/java/com/tam/challenge/bearpictures/BearpicturesApplication.java
@@ -0,0 +1,13 @@
+package com.tam.challenge.bearpictures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BearpicturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BearpicturesApplication.class, args);
+	}
+
+}
